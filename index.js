071fcc22ca3ab66f3d90cef353693a13598fcd36
Grace Dian <gracedian28902@gmail.com>
2019-09-05 00:23:52 +0700

    test

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World');
-})
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('{"success ":true", "data": "index", "message": "Hello World", "code": 200}');
+});
 
 app.listen(process.env.PORT || 4000, function() {
     console.log('Your node js server is running');
